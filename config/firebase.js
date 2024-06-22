0cb2c9c77cceace1c0591882fc906751d8c190f6
studyarrahman <studyarrahman552778@gmail.com>
2024-06-22 19:17:37 +0700

    hide credentials

--- config/firebase.js
+++ config/firebase.js
@@ -7,7 +7,7 @@ const secretManagerClient = new SecretManagerServiceClient();
 
 async function getSecret(secretName) {
   const [version] = await secretManagerClient.accessSecretVersion({
-    name: `projects/hay-hair-beauty/secrets/${secretName}/versions/latest`,
+    name: `projects/${process.env.GCLOUD_PROJECT_ID}/secrets/${secretName}/versions/latest`,
   });
 
   const payload = version.payload.data.toString('utf8');
@@ -18,23 +18,14 @@ async function initializeFirebase() {
   if (!getApps().length) {
     try {
       const serviceAccount = await getSecret('my-serviceaccountkey');
+      const firebaseConfig = await getSecret('my-firebase-config');
 
       admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
-        databaseURL: "https://hay-hair-beauty.firebaseio.com",
+        databaseURL: firebaseConfig.databaseURL,
       });
 
       const db = admin.firestore();
-
-      const firebaseConfig = {
-        apiKey: "AIzaSyCXr2cwdIn-l2AKnVcz5eAGGhxQc3OGpeM",
-        authDomain: "hay-hair-beauty.firebaseapp.com",
-        projectId: "hay-hair-beauty",
-        storageBucket: "hay-hair-beauty.appspot.com",
-        messagingSenderId: "126780028253",
-        appId: "1:126780028253:web:d0386b51b13f49443124b4",
-        measurementId: "G-5QJHMN455P",
-      };
 
       // Initialize Firebase
       const app = initializeApp(firebaseConfig);
