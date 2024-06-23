cdf390d16ae4ad4b607c75251af4c91e9d908f75
studyarrahman <studyarrahman552778@gmail.com>
2024-06-23 12:35:10 +0700

    bug search

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -101,16 +101,24 @@ const searchArticles = async (req, res) => {
   const page = req.query.page ? parseInt(req.query.page) : 1;
   const limit = req.query.limit ? parseInt(req.query.limit) : 20;
 
-  if (!title) {
-    return res.status(400).json({ error: 'Title is required for search' });
-  }
-
   try {
+    if (!title || title.trim() === '') {
+      // If title is not provided or is empty, get all articles
+      const { articles, currentPage, totalPages, totalItems, itemsPerPage } = await ArticleService.getAllArticles(page, limit);
+      return res.json({
+        data: articles,
+        currentPage,
+        totalPages,
+        totalItems,
+        itemsPerPage
+      });
+    }
+
     const lowerCaseTitle = title.toLowerCase();
     console.log('Received search request for title:', lowerCaseTitle);
-    
+
     const { articles, currentPage, totalPages, totalItems, itemsPerPage } = await ArticleService.searchArticlesByTitle(lowerCaseTitle, page, limit);
-    
+
     if (articles.length === 0) {
       return res.status(404).json({ message: 'Data tidak ditemukan' });
     }
@@ -135,6 +143,7 @@ const searchArticles = async (req, res) => {
     res.status(500).json({ error: 'Failed to search articles' });
   }
 };
+
 
 
 
